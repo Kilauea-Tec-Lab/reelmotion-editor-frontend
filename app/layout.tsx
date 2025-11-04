6d95703af476f52f0360e949c563b710f142f6a9
Dekepdevelop <dekepdevelop@gmail.com>
2025-11-03 19:11:26 -0600

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
-  title: "React Video Editor | Pro",
-  description: "Purchased version of the React Video Editor.",
+  title: "Reelmotion Editor",
+  description: "Reelmotion Editor",
+  icons: {
+    icon: "/icons/icon_reelmotion_ai.png",
+    shortcut: "/icons/icon_reelmotion_ai.png",
+    apple: "/icons/icon_reelmotion_ai.png",
+  },
 };
 
 export default function RootLayout({
--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -8,14 +8,14 @@ export default function Navbar() {
     <nav className="border-gray-200 bg-gray-900 border-b">
       <div className="mx-auto md:px-12 lg:px-20 max-w-7xl relative">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="https://www.reactvideoeditor.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img
-              src="/icons/logo.svg"
+              src="/icons/icon_reelmotion_ai.png"
               className="h-9"
-              alt="React Video Editor Logo"
+              alt="Reelmotion"
             />
             <span className="self-center text-xl md:text-2xl font-light whitespace-nowrap text-white">
-              RVE
+              Reelmotion
             </span>
           </Link>
         
